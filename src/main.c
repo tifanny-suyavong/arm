4bc178fc748b36672b4d0a298efdd5655c9eb6be
Tifanny Suyavong <tifanny.suyavong@epita.fr>
2018-06-04 18:58:10 +0200

    main: add gpios addresses

--- src/main.c
+++ src/main.c
@@ -1,31 +1,36 @@
-#define RCC_AHB1ENR 0x40023800 + 0x10 // Reset and clock controller
+#define RCC_AHB1ENR  (0x40023800 + 0x10) // Reset and clock controller
 
-#define GPIO_MODER  0x0               // TODO: Find gpio base
-#define GPIO_IDR    GPIO_MODER + 0x10
-#define GPIO_ODR    GPIO_MODER + 0x14
+// User button
+#define GPIOA_MODER  0x40020000 
+#define GPIOA_IDR    (GPIOA_MODER + 0x10)
 
-void break_here(void)
-{
-  return;
-}
+// Led LD3
+#define GPIOG_MODER  0x40021800
+#define GPIOG_ODR    (GPIOG_MODER + 0x14)
+
+#define PORTA_MODE    0x0
+#define PORTG_MODE    0x1
 
 int main(void)
 {
-  while (1)
-    break_here();
+    volatile int *rcc = (volatile int *) RCC_AHB1ENR;
+    volatile int *gpioa_mode = (volatile int *) GPIOA_MODER;
+    volatile int *gpiog_mode = (volatile int *) GPIOG_MODER;
+    volatile int *button_input_data = (volatile int *) GPIOA_IDR;
+    volatile int *led_output_data = (volatile int *) GPIOG_ODR;
 
-  volatile int *rcc = (volatile int*) RCC_AHB1ENR;
-  volatile int *gpio_mode = (volatile int*) GPIO_MODER;
-  volatile int *gpio_input_data = (volatile int*) GPIO_IDR;
-  volatile int *gpio_output_data = (volatile int*) GPIO_ODR;
-
-  // Enable all the GPIOs
-  *rcc = *rcc | 2047;
-  // TODO: Find gpio to enable write mode for ODR
-  *gpio_mode = *gpio_mode | 0x0;
-  // TODO: Find value for pushed button
-  if (*gpio_input_data & 0x0)
-      // TODO: Find value to enable led
-      *gpio_output_data = *gpio_output_data & 0x0;
-  return 0;
+    // Enable all the GPIOs
+    *rcc = *rcc | ((1 << 11) - 1);
+  
+    // TODO: Find gpio to enable write mode for ODR
+    *gpioa_mode = *gpioa_mode | PORTA_MODE;
+    *gpiog_mode = *gpiog_mode | PORTG_MODE;
+    
+    // TODO: Find value for pushed button
+    if (*button_input_data & 0x1)
+        // TODO: Find value to enable led
+        *led_output_data = *led_output_data & (1 << 12);
+    else
+        *led_output_data = *led_output_data & 0x0;
+    return 0;
 }
